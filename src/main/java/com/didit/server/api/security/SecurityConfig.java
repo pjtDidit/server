acd8911119730d4b52ceb09b22d78ebbe6dd7a69
Kim Yi <95015653+eaststar113@users.noreply.github.com>
2026-01-21 15:56:25 +0900

    #3 add logout logic (#25)
    Co-authored-by: yi <onething42@naver.com>

--- src/main/java/com/didit/server/api/security/SecurityConfig.java
+++ src/main/java/com/didit/server/api/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.didit.server.api.security;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,14 +21,19 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/api/v1/auth/login", "/login/**", "/oauth2/**").permitAll()
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/", "/api/v1/auth/login", "/api/v1/auth/logout","/login/**", "/oauth2/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2Login(oauth2 -> oauth2
                         .userInfoEndpoint(userInfo -> userInfo.userService(customOAuth2UserService))
                         .defaultSuccessUrl("/"))
                 .logout(logout -> logout
                         .logoutUrl("/api/v1/auth/logout")
-                        .invalidateHttpSession(true)
+                        .addLogoutHandler((request, response, authentication) -> {
+                            HttpSession session = request.getSession();
+                            session.invalidate();
+                        })
+                        .logoutSuccessHandler((request, response, authentication) ->
+                                response.sendRedirect("/")) //리다이렉트 될 페이지 추후 수정 필요
                         .deleteCookies("JSESSIONID")
                         .logoutSuccessUrl("/")
                 );
